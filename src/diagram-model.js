738cea1797cb3f9e7bfc77373ba320d2683bd1ab
Joonseok Hur <joonseok.hur@wisc.edu>
2026-04-15 16:46:24 -0500

    Add MPL notice blocks to frontend source files

--- src/diagram-model.js
+++ src/diagram-model.js
@@ -1,13 +1,15 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Copyright (c) 2026 Joonseok Hur
- *
- * Originally developed by Joonseok Hur in the Josiah Sinclair Group,
- * UW-Madison.
- */
++--------------------------------------------------------------------+
+| This Source Code Form is subject to the terms of the Mozilla Public |
+| License, v. 2.0. If a copy of the MPL was not distributed with this |
+| file, You can obtain one at https://mozilla.org/MPL/2.0/.           |
+|                                                                     |
+| Copyright (c) 2026 Joonseok Hur                                     |
+|                                                                     |
+| Originally developed by Joonseok Hur in the Josiah Sinclair Group,  |
+| UW-Madison.                                                         |
++--------------------------------------------------------------------+
+*/
 
 function groupIntegerDigits(integerText) {
   const sign = integerText.startsWith("-") || integerText.startsWith("+") ? integerText[0] : "";
--- src/diagram-render.js
+++ src/diagram-render.js
@@ -1,13 +1,15 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/.
- *
- * Copyright (c) 2026 Joonseok Hur
- *
- * Originally developed by Joonseok Hur in the Josiah Sinclair Group,
- * UW-Madison.
- */
++--------------------------------------------------------------------+
+| This Source Code Form is subject to the terms of the Mozilla Public |
+| License, v. 2.0. If a copy of the MPL was not distributed with this |
+| file, You can obtain one at https://mozilla.org/MPL/2.0/.           |
+|                                                                     |
+| Copyright (c) 2026 Joonseok Hur                                     |
+|                                                                     |
+| Originally developed by Joonseok Hur in the Josiah Sinclair Group,  |
+| UW-Madison.                                                         |
++--------------------------------------------------------------------+
+*/
 
 function getHyperfineScalePxPerMHz(fineState) {
   const localScale = getHyperfineScaleForFineState(fineState.id);
